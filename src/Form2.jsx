ffbb8f2ed47818fc669f79e0233b55cb8c33ced9
sayali singh <sayalibaghel6@gmail.com>
2025-09-28 23:44:52 +0530

    new

---
+++ src/Form2.jsx
@@ -0,0 +1,103 @@
+ import React, { useState } from 'react'
+
+// const Form2 = () => {
+    
+//         // const [name,setname]=useState(" ")
+//         // const [email,setemail]=useState(" ")
+//         // const [num,setnum]=useState(" ")
+//         // const [pass,setpass]=useState(" ")
+       
+//         const[formdata,setformdata]=useState({
+//             name:"",
+//             email:"",
+//             num:"",
+//             pass:""
+//         })
+
+//     let handlesubmit=(e)=>{
+//       // preventDefault to stop form reloading or refresh
+//       e.preventDefault()
+//       localStorage.setItem("username",formdata.name)
+//       localStorage.setItem("useremail",formdata.email)
+//       localStorage.setItem("usernum",formdata.num)
+//       localStorage.setItem("userpass",formdata.pass)
+//     }
+
+//     const changeinp=(e)=>{
+//         setformdata({
+//             ...formdata,
+//             [e.target.name]:e.target.value
+//         }
+//         )
+//     }
+
+//   return (
+//    <>
+//    <form onSubmit={handlesubmit}>
+      
+//     Enter name:<input type="text" value={formdata.name} onChange={changeinp} name='name'/> <br /> <br />
+    
+//      Enter email:<input type="text" value={formdata.email} onChange={changeinp}
+//      name='email'/> <br /> <br />
+     
+//     Enter number:<input type="text" name='num' value={formdata.num} onChange={changeinp}/> <br /> <br />
+    
+//      Enter password:<input type="text" name='pass' value={formdata.pass} onChange={changeinp}/> <br /> <br />
+//     <button type='submit'>Submit</button>
+//     </form>
+//    </>
+//   )
+// }
+
+
+
+const Form2 = () => {
+    
+        // const [name,setname]=useState(" ")
+        // const [email,setemail]=useState(" ")
+        // const [num,setnum]=useState(" ")
+        // const [pass,setpass]=useState(" ")
+       
+        const[formdata,setformdata]=useState({
+            name:"",
+            email:"",
+            num:"",
+            pass:""
+        })
+
+    let handlesubmit=(e)=>{
+      // preventDefault to stop form reloading or refresh
+      e.preventDefault()
+      localStorage.setItem("username",formdata.name)
+      localStorage.setItem("useremail",formdata.email)
+      localStorage.setItem("usernum",formdata.num)
+      localStorage.setItem("userpass",formdata.pass)
+    }
+
+    const changeinp=(e)=>{
+        setformdata({
+            ...formdata,
+            [e.target.name]:e.target.value
+        }
+        )
+    }
+
+  return (
+   <>
+   <form onSubmit={handlesubmit}>
+      
+    Enter name:<input type="text" value={formdata.name} onChange={changeinp} name='name'/> <br /> <br />
+    
+     Enter email:<input type="text" value={formdata.email} onChange={changeinp}
+     name='email'/> <br /> <br />
+     
+    Enter number:<input type="text" name='num' value={formdata.num} onChange={changeinp}/> <br /> <br />
+    
+     Enter password:<input type="text" name='pass' value={formdata.pass} onChange={changeinp}/> <br /> <br />
+    <button type='submit'>Submit</button>
+    </form>
+   </>
+  )
+}
+
+export default Form2
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Props1.jsx'
+import App from './Form2.jsx'
 
 
 
