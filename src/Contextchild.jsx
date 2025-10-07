5bbc50767879bd30c841a49ab20b64735983cc12
sayali singh <sayalibaghel6@gmail.com>
2025-10-07 17:06:12 +0530

    n

--- src/Contextchild.jsx
+++ src/Contextchild.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import mycontext from './mycontext'
 import { useContext } from 'react'
+import { useState } from 'react'
 
 const Contextchild = () => {
 
-    let mydata = useContext(mycontext)
+    // let mydata = useContext(mycontext)
+    //  let {mytheme ,array} = useContext(mycontext)
+     let num = useContext(mycontext)
+      let mytheme = useContext(mycontext)
 
+       const[mode ,setmode] = useState(false)
+          
+          
+      
+      //     mode ?mode=<h1>dark</h1>  :mode=<h1>light</h1>
   return (
     <>
     
-    <div>my name is {mydata.name}</div>
+    {/* <div>my name is {mydata.name}</div> */}
+    {/* <div style = {mytheme.dark}>hhh</div>
+    <h1>  my hobbies are  {array}</h1> */}
+    {/* <h1>{num.num1 +num .num2}</h1> */}
+    
+    <div style = {mode?mytheme.dark:mytheme.light}> hii</div>
+    <button onClick={()=> {setmode(!mode)}}>click</button> 
+
+    
+
+
+
     </>
   )
 }
 
 export default Contextchild
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,35 @@
-// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./contextmain.jsx"
 import mycontext from "./mycontext.jsx";
 
 
-let mydata ={
-    name :"say"
+// let mydata ={
+//     name :"say"
+// }
+
+let mytheme ={
+    dark :{"background-color":'black' ,"color":"white"},
+    light :{"background-color":'white'}
 }
 
+// let array = [ "palying" ," ","dancing"]
+
+// let num = { "num1" :2 ,"num2":5}
+
+
+// desr
+
+// let alldata = {mydata ,mytheme,array}
+
+
 createRoot(document.getElementById('root')).render(
-  <mycontext.Provider value={mydata}>
+  // <mycontext.Provider value={{mytheme ,array}}>
+  <mycontext.Provider value={mytheme}>
+    <BrowserRouter>
       <App />
+      </BrowserRouter>
   </mycontext.Provider>
    
   
